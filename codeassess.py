18b26cfd3dffe99f658ceeef1677e7b1aae21616
jbell98 <justin.bell1@bison.howard.edu>
2016-10-14 12:45:52 -0400

    Update and rename step1.py to codeassess.py

---
+++ codeassess.py
@@ -0,0 +1,10 @@
+#step 1: Registration
+import json
+import requests
+
+def registration():
+  url = "http://challenge.code2040.org/api/register"
+  github = "https://github.com/jbell98/code2040techassess"
+  token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
+  
+
