bc8e8ac5f7ad2c22bcbc1201b0c86ecd50746d3a
jbell98 <justin.bell1@bison.howard.edu>
2016-10-27 11:28:28 -0400

    Update codeassess.py

--- codeassess.py
+++ codeassess.py
@@ -4,7 +4,8 @@ import requests
 url = "http://challenge.code2040.org/api/register"
 github = "https://github.com/jbell98/code2040techassess"
 token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
-total = requests.post(url, json={'token': token, 'github': github})
+x={'token': token, 'github': github}
+total = requests.post(url, json=x)
 print(total)
 print(token, github)
 print(total.text)
@@ -12,25 +13,11 @@ total.json()
 
 
 #step 2: Reverse string
+def reverse_str(text):
+    url1 = 'http://challenge.code2040.org/api/reverse'
+    url2 = 'http://challenge.code2040.org/api/reverse/validate'
 
-def Anagram(str1, str2):
-    my_list = list(str2)
-    num = 0
-    if str1 == '' or "":
-        return False
-    while num <len(str1) and True:
-        x = 0
-        found = False
-        while x < len(my_list) and not found:
-            if str1(num) == my_list(x):
-                found = True
-            else:
-                x+=1
-        if found:
-            my_list[x] = None
-        else:
-            return False
-
-        num +=1
-    return True
-print(Anagram('anna', 'racecar')
+    x["string"] = total.text[::-1]
+    total = requests.post(url1, url2, json=x)
+    print(total.text)
+return reverse_str()
