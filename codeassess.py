a0aaca8372d9315b8b7918f5d6116cde79b26694
jbell98 <justin.bell1@bison.howard.edu>
2016-10-26 23:15:21 -0400

    Update codeassess.py

--- codeassess.py
+++ codeassess.py
@@ -1,10 +1,11 @@
 #step 1: Registration
 import json
 import requests
-
-def registration():
-  url = "http://challenge.code2040.org/api/register"
-  github = "https://github.com/jbell98/code2040techassess"
-  token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
-  
-
+url = "http://challenge.code2040.org/api/register"
+github = "https://github.com/jbell98/code2040techassess"
+token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
+total = requests.post(url, json={'token': token, 'github': github})
+print(total)
+print(token, github)
+print(total.text)
+total.json()
