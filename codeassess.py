5e1f274cec31c66adf8451b0d0dd725aed57bbeb
jbell98 <justin.bell1@bison.howard.edu>
2016-10-31 09:41:22 -0400

    Update codeassess.py

--- codeassess.py
+++ codeassess.py
@@ -1,25 +1,32 @@
-#step 1: Registration
 import json
 import requests
+url = "http://challenge.code2040.org/api/register"
+github = "https://github.com/jbell98/code2040techassess"
+token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
 def registration():
-    url = "http://challenge.code2040.org/api/register"
-    github = "https://github.com/jbell98/code2040techassess"
-    token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
+
     x={'token': token, 'github': github}
     total = requests.post(url, json=x)
     print(total)
     print(token, github)
     print(total.text)
-total.json()
+print(registration())
 
-
+url1 = 'http://challenge.code2040.org/api/reverse'
+url2 = 'http://challenge.code2040.org/api/reverse/validate'
+token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
 #step 2: Reverse string
-def reverse_str(token):
-    url1 = 'http://challenge.code2040.org/api/reverse'
-    url2 = 'http://challenge.code2040.org/api/reverse/validate'
-    token = "1b0d064d7f3fbe9bc9ab1a6ed56dc8a8"
-    x["string"] = total.text[::-1]
+def reverse_str():
+    #x["string"] = total.text[::-1]
+    x={'token': token, 'github': github}
     total = requests.post(url1, url2, json=x)
-    print(token, url1, url2)
-    print(total.text)
-return reverse_str(token)
+    print (total.content)
+    text = total.content
+    string = text[::-1]
+    print(string)
+    total2 = requests.post(url2, json=x)
+    print(total2.text)
+print( reverse_str())
+
+
+#step 3:
